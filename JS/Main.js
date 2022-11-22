30cc85a46fe8bcc6ff8de036f9a5ececaa11d3b8
drewmania <justinhot906@gmail.com>
2022-11-22 16:17:00 -0500

    cart feature

---
+++ JS/Main.js
@@ -0,0 +1,10 @@
+import { amount } from "./cart.js";
+
+document.getElementById("amountBtn").addEventListener('click', e=> {
+    amount();
+}) 
+document.getElementById("amountBtn").addEventListener('keydown', e=> {
+    amount();
+}) 
+
+ 
